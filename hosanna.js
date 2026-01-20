b6e1e6cd12e43f6e18551cd7063746085f9678ca
illia-butkowski <iliabutkovski@gmail.com>
2026-01-20 23:05:10 +0200

    Create hosanna.js

---
+++ hosanna.js
@@ -0,0 +1,98 @@
+const song_hosanna = {
+    id: "hosanna",
+    youtubeId: "NoM0AT8fBvs", // Hillsong United - Hosanna
+    uk: {
+        title: "Осанна",
+        artist: "Hillsong / Brooke Ligertwood",
+        content: `КУПЛЕТ 1:
+ 4 4   -3    4   -5  -6 6
+Я бачу як Цар Слави, 
+ 4   4   4   -4   4  -5 6
+ось гряде до нас з вогнем
+  4   -6   5   -2     4   -6   5   -4
+Тремтить усе,      тремтить усе
+ 4 4   -3    4   -5  -6 6
+Я бачу як в любові, 
+ 4  4 4 -4 4 -5 6
+гріхи омиє Він
+ 4  -6   5  -2      4  -6   5   -4
+Земля співа,       земля співа!
+
+ПРИСПІВ:
+ 5   6  -6     5   6  -6
+Осанна,       Осанна, 
+ 5   6  -5 5   -4  5  -4
+Осанна Богу ввишніх!
+ 5   6  -6     5   6  -6
+Осанна,       Осанна, 
+ 4  -6   6 -5 5  -5   6  5
+Осанна Богу  ввишніх!
+
+КУПЛЕТ 2:
+ 4 4   -3    4   -5  -6 6
+Я бачу покоління, 
+ 4   4 4 -4 4 -5 6
+з вірою воно встає
+  4   -6  5  -2      4   -6  5  -4
+знайти Тебе,      знайти Тебе
+ 4 4   -3    4   -5  -6 6
+Я бачу як в молитві, 
+ 4   4 4 -4 4 -5 6
+пробудження прийде
+  4   -6  5   -2      4   -6  5   -4
+Вклониться все,    Вклониться все
+
+БРІДЖ:
+-6  -6  -6 -6  -6   6 5 -4
+Серце Дух зціли, омий
+ -5  -5 -5   5  -4   -4 5  -5   5   -2
+Мир небачений, очі ти відкрий
+ -6  -6  -6 -6   6   5    4  -5  5
+Навчи любити, так як Ти всіх любиш
+
+-6  -6  -6  -6 -6   6  5  -4
+Дай відчути Бог Твій біль
+ -5  -5  -5  5  -4 -4 5 -5 5 -2
+Віддам своє життя, в руки Твої
+ -6 -6  -6 -6    6   5   4 -5 5
+Так пройду свій шлях земний до Тебе`
+    },
+    en: {
+        title: "Hosanna",
+        artist: "Hillsong United",
+        content: `VERSE 1:
+ 4 4  -3   4   -5  -6 6
+I see the King of glory
+ 4  4   4  -4    4    -5  6
+Coming on the clouds with fire
+ 4   -6   5    -2      4   -6   5    -4
+The whole earth shakes, the whole earth shakes
+ 4 4  -3   4    -5  -6 6
+I see His love and mercy
+ 4 4  4 -4  4   -5  6
+Washing over all our sin
+ 4  -6  5   -2      4  -6  5   -4
+The people sing,    the people sing
+
+CHORUS:
+ 5   6  -6     5   6  -6
+Hosanna,      Hosanna,
+ 5   6  -5 5  -4  5   4
+Hosanna in the highest!
+
+BRIDGE:
+-6   -6   -6   -6  -6    6 5  -4
+Heal my heart and make it clean
+ -5 -5  -5  5  -4  -4 5   -5    5   -2
+Open up my eyes to the things unseen
+-6   -6  -6  -6  6    5    4  -5   5
+Show me how to love like You have loved me
+
+-6   -6   -6   -6   -6   6  5   -4
+Break my heart for what breaks Yours
+ -5 -5 -5 5 -4  -4  5 -5   5    -2
+Everything I am for Your kingdom's cause
+-6 -6 -6  -6   6   5   4 -5 5
+As I walk from earth into eternity`
+    }
+};
