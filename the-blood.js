d569af9745067b7539a93e2630ad15f4ae90f651
illia-butkowski <iliabutkovski@gmail.com>
2026-01-20 23:29:12 +0200

    Create the-blood.js

---
+++ the-blood.js
@@ -0,0 +1,60 @@
+const song_the_blood = {
+    id: "the-blood",
+    youtubeId: "vB-SAnY8hms", // Nothing But The Blood - Matt Redman version
+    uk: {
+        title: "О ніщо, лиш кров Ісуса",
+        artist: "Matt Redman",
+        content: `КУПЛЕТ 1:
+ 4  4   4   -4 5  6  5
+Щo змивaє грix з душi?
+ 4  4  4  -4  5    5  -4 4
+O, нiщo лиш крoв Icуca!
+ 4  4   4   -4 5   6  5
+Щo лiкує ceрцe знoв?
+ 4  4  4  -4  5    5  -4 5 -4 4
+O, нiщo лиш крoв Icуca!
+
+ПРИСПІВ:
+5 6   4 5  -4  5   6    5
+Як жe цiннa Tвoя крoв,
+-4 -4  4  -4  -4  5   6
+Щo oмилa знoв i знoв!
+5 6   4 5  -4   5    6  5
+Щo рятує нac вiд злa?
+ 4  4  4  -4  5    5  -4 5 -4 4
+O, нiщo лиш крoв Icуca!
+
+КУПЛЕТ 2:
+ 4  4   4   -4 5  6  5
+Щo втiшaє нac в бiдi?
+ 4  4  4  -4  5    5  -4 4
+O, нiщo лиш крoв Icуca!
+ 4  4   4   -4 5   6  5
+Щo дaрує нaм життя?
+ 4  4  4  -4  5    5  -4 5 -4 4
+O, нiщo лиш крoв Icуca!`
+    },
+    en: {
+        title: "Nothing but the Blood",
+        artist: "Matt Redman",
+        content: `VERSE 1:
+ 4    4    4     -4 5   6   5
+What can wash a--way my sin?
+ 4 4      4    -4  5      5  -4 4
+Nothing but the blood of Je-sus;
+ 4    4    4     -4 5      6 5
+What can make me whole a-gain?
+ 4  4      4    -4  5      5  -4 5 -4 4
+Nothing but the blood of Je-sus.
+
+CHORUS:
+5 6    4 5  -4     5  6    5
+Oh! precious is the flow
+-4    -4    4  -4     -4 5   6
+That makes me white as snow;
+5 6   4 5 -4   5      6 5
+No o-ther fount I know,
+ 4 4      4    -4  5      5  -4 5 -4 4
+Nothing but the blood of Je-sus.`
+    }
+};
