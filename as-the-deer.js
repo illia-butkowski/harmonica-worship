a230438a3abfb6d77b6b394649f39817b7f54409
illia-butkowski <iliabutkovski@gmail.com>
2026-01-20 17:53:35 +0200

    Update as-the-deer.js

--- as-the-deer.js
+++ as-the-deer.js
@@ -5,50 +5,51 @@ const song_as_the_deer = {
         title: "Як олень прагне",
         artist: "Martin Nystrom",
         content: `
-        5  6    6    5  -4   4  -4  -5  5 
+        5  6  6  5 -4 4  -4 -5 5 
         Як олень прагне до потоку,
-        -4  4  -6   -6  -6   6 -5    6
+        -4  4 -6  -6 -6 6 -5  6
         Так душа моя прагне Тебе!
-        5  6   6   5  -4   4     -4 -5 5
+        5  6   6   5  -4 4 -4 -5 5
         Я бажаю Тобі сказати
-       -4  4   4   5 -4    4    4 
+       -4  4   4   5 -4  4 4 
         Що я так люблю Тебе.
 
-        7  7   7   7  -7    -6     -6    6 
+        7  7   7   7  -7 -6  -6  6 
         Ти один моє серце знаєш,
-        4 -6  -6  -6   -6  6   -5  -5   5
+        4 -6  -6  -6   -6  6 -5 -5 5
         І в нього Ти любов вливаєш.
-        5  6   6   5  -4   4     -4 -5 5
+        5  6 6 5  -4 4  -4 -5 5
         Я бажаю Тобі сказати
-       -4  4   4   5 -4    4    4
+       -4  4 4 5 -4  4  4
         Що я так люблю Тебе.`
     },
     en: {
         title: "As the Deer",
         artist: "Martin Nystrom",
-        content: ` 5  6    6    5  -4   4  -4  -5  5
+        content: ` 5  6  6  5  -4 4  -4  -5  5
 As the deer pant-eth for the wa-ter,
 
--4  4  -6   -6  -6   6 -5    6
+-4  4  -6   -6  -6   6 -5  6
 So my soul long-eth af-ter Thee.
 
- 5  6   6   5  -4   4     -4 -5 5
+ 5  6   6   5  -4   4   -4 -5 5
 You a-lone are my heart's de-sire,
 
 -4  4   4   5 -4    4    4
 And I long to wor-ship Thee.
 
- 7  7   7   7  -7    -6     -6    6 
+ 7  7   7   7  -7    -6    -6    6 
 You a-lone are my strength, my shield;
 
  4 -6  -6  -6   -6  6   -5  -5   5
 To You, a-lone, may my spir-it yield.
 
- 5  6   6   5  -4   4     -4 -5 5
+ 5  6   6   5  -4   4    -4 -5 5
 You a-lone are my heart's de-sire,
 
 -4  4   4   5 -4    4    4
 And I long to wor-ship Thee.`
     }
 };
+
 
