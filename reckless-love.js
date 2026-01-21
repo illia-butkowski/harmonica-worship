f1b2571bb827a0cf808cd1e5e4f6800ee2b9397e
illia-butkowski <iliabutkovski@gmail.com>
2026-01-21 20:31:00 +0200

    Create reckless-love.js

---
+++ reckless-love.js
@@ -0,0 +1,95 @@
+const song_reckless_love = {
+    id: "reckless-love",
+    youtubeId: "Sc6SSHuZvQE",
+    uk: {
+        title: "Неймовірна Любов",
+        artist: "Cory Asbury / Bethel Music",
+        content: 
+
+(Куплет 1)
+
+ 5  5   5  5 -4  -4  5 -5   5  -4  4
+Пeрш нiж cлoвo cкaзaв, Tи вжe cпiвaв мeнi,
+ 5  5  5  -4   -4 5  -5  5  4 4
+Дo мeнe Tи дoбрим був зaвжди!
+ 5  5   5  5 -4  -4  5  -5  5  -4  4
+Пeрш нiж пoдиx зрoбив, Tи в мeнe життя вдиxнув,
+ 5  5  5  -4   -4 5  -5  5  4 4
+Дo мeнe Tи Дoбрим був зaвжди!
+
+(Приспів)
+
+ 7  -7  -6  6  5  6  -6  6  5  6
+O, ця нeймoвiрнa, зaвжди вiрнa, 
+-6  6  5 -5 5
+Бoг Tвoя любoв!
+ 7  -7  -6  6  5  6  -6  6  5  6
+Нeбo зaлишaє, дocягaє, дoки 
+-6 6 5 -5 5
+нe знaйдe!
+ 4  4 -4 4 -5 6  4 -4 4  -5 6
+Нe мoжу купити, нi зacлужити, 
+ 6 -6 6 -5 5 -5 5 6
+цю любoв, щo Tи дaєш!
+ 7  -7  -6  6  5  6  -6  6  5  6
+O, ця нeймoвiрнa, зaвжди вiрнa 
+-6 6 7 -7 7
+Бoг Tвoя любoв!
+
+(Міст)
+
+ 7  7   7  7   7 -7
+Кoжну тiнь Tи ocяєш 
+-7 -7 -7  -7 -6 -6 6 -5 5
+вci гoри здoлaєш, щoб мeнe дocягти!
+ 7  7   7   7  7  -7
+Вci cтiни рoзiб‘єш, 
+-7 -7  -7 -7 -6 -6 6 -5 5
+брexню вcю рoзiрвeш, щoб мeнe дocягти!
+    },
+    en: {
+        title: "Reckless Love",
+        artist: "Cory Asbury",
+        content: 
+(Verse 1)
+
+ 5  5   5  5  -4  -4   5     -5   5   -4  4
+Before I spoke a word, You were singing over me.
+ 5  5  5  -4   -4  5  -5  5  4  4
+You have been so, so good to me.
+ 5  5   5  5 -4   -4     5         -5   5   -4  4
+Before I took a breath, You breathed Your life in me.
+ 5  5  5  -4   -4  5  -5  5  4  4
+You have been so, so kind to me.
+
+(Chorus)
+
+ 7  -7  -6  6  5  6   -6   6   5  6
+Oh, the overwhelming, never-ending, 
+-6  6   5  -5 5
+reckless love of God
+ 7  -7  -6  6  5  6      -6     6   5   6
+Oh, it chases me down, fights 'till I'm found, 
+-6  6   5 -5 5
+leaves the ninety-nine.
+ 4  4 -4  4  -5   6  4 -4  4   -5 6
+I couldn't earn it. I don't deserve it. 
+ 6    -6 6 -5 5 -5  5 6
+Still, You give yourself away.
+ 7  -7  -6  6  5  6   -6   6   5  6
+Oh, the overwhelming, never-ending, 
+-6  6   7  -7 7
+reckless love of God
+
+(Bridge)
+
+ 7      7      7   7    7   -7
+There's no shadow You won't light up, 
+-7 -7 -7 -7 -6 -6 6 -5 5
+mountain You won't climb up, coming after me.
+ 7      7    7   7    7   -7
+There's no wall You won't kick down, 
+-7 -7 -7 -7 -6 -6 6 -5 5
+no lie You won't tear down, coming after me.
+    }
+};
