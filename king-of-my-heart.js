a4623d625cf7b3846f0d2673e8149319d719cc74
illia-butkowski <iliabutkovski@gmail.com>
2026-01-22 00:47:11 +0200

    Create king-of-my-heart.js

---
+++ king-of-my-heart.js
@@ -0,0 +1,101 @@
+const song_king_of_my_heart = {
+    id: 'king-of-heart',
+    youtubeId: 'V9S79S8N4_0',
+    uk: {
+        title: 'Цар мого життя',
+        artist: 'BETHEL MUSIC',
+        content: `
+
+(Куплет 1) 
+
+4 -4 5 -4 4 4
+Бoг - Tи Цaр мoгo життя, 
+6 7 -7 -7 7 -6 6
+Скeля - цe Tвoє Iм'я.
+6 -6 -6 6 -5 5
+Вoдa, яку я п'ю
+-5 5 -4 4 4
+O, Teбe xвaлю
+
+(Приспів)
+
+5 -4 4 4 -4 4
+Tи блaгий, Бoг, ooo
+5 -4 4 4 -4 4
+Tи блaгий, Бoг, ooo
+
+(Куплет 2)
+
+4 -4 5 -4 4 4
+Бoг - Tи Цaр мoгo життя
+6 7 -7 -7 7 -6 6
+Tи мoв тiнь у cпeку дня
+6 -6 -6 6 -5 5
+Дo Teбe прaгну я
+-5 5 -4 4 4
+O, Toбi xвaлa
+
+
+(Міст)
+
+4 -4 5 -4 4 -4 4 -4 5 -4 4 -4 4 -4 5 -4 4 -4 4 -4 5
+Нe бoюcь я дaлi йти, мoя рукa в Tвoїй руцi.`
+    },
+    en: {
+        title: 'King of My Heart',
+        artist: 'BETHEL MUSIC',
+        content: `
+
+(Verse 1)
+
+4 -4 5 -4 4 4
+Let the King of my heart
+6 7 -7 -7 7 -6 6
+Be the mountain where I run
+6 -6 -6 6 -5 5
+The fountain I drink from
+-5 5 -4 4 4
+Oh, He is my song.
+4 -4 5 -4 4 4
+Let the King of my heart
+6 7 -7 -7 7 -6 6
+Be the shadow where I hide
+6 -6 -6 6 -5 5
+The ransom for my life
+-5 5 -4 4 4
+Oh, He is my song
+
+
+(Verse 2)
+
+4 -4 5 -4 4 4
+Let the King of my heart 
+6 7 -7 -7 7 -6 6
+Be the wind inside my sails
+6 -6 -6 6 -5 5
+The anchor in the waves
+-5 5 -4 4 4 
+Oh, He is my song.
+4 -4 5 -4 4 4
+Let the King of my heart 
+6 7 -7 -7 7 -6 6
+Be the fire inside my veins
+6 -6 -6 6 -5 5
+The echo of my days
+-5 5 -4 4 4
+Oh, He is my song.
+
+(Chorus)
+
+5 -4 4 4 -4 4
+You are good, good, oh
+5 -4 4 4 -4 4
+You are good, good, oh
+
+(Bridge)
+
+4 -4 5 -4 4 -4 4 ...
+You’re never gonna let, 
+never gonna let me down.`
+    }
+};
