3fb549b1fe52d57ce67d7d95d0c7ea7324c7b7ff
illia-butkowski <iliabutkovski@gmail.com>
2026-01-22 22:37:17 +0200

    Create i-know-a-name.js

---
+++ i-know-a-name.js
@@ -0,0 +1,381 @@
+const song_i_know_a_name = {
+    id: "i-know-a-name",
+    youtubeId: "Q_TVlSYq_LM", // Elevation Worship - I Know A Name
+    bpm: 86,
+    uk: {
+        title: "Я знаю Ім'я",
+        artist: "Elevation Worship",
+        content: `
+        
+(Куплет 1)
+  
+4 -4 4 5  4 -4 4 -5 5 -4 4 4
+Я знаю Ім'я, що руйнує хвилі всі
+4 -4 4 5  4 -4 4 -5 5 -4 4 4
+Я знаю Ім'я, що спроможне воскресить
+4 -4 4 5  4 -4 4 -5 5 -4 4 4
+Я знаю Ім'я, що спасає нас усіх 
+4 -4 4 -5 5 -4 4 4
+Воно гідне всій хвали!
+
+(Приспів)
+
+4 -4 4 5 5
+Я кличу Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Цілитель
+-5 5 -4 -5 5 -4 6 6
+Воскрес і царюєш у силі 
+4 -4 5 -5 5 -4 -4
+Серед усіх царств земних
+
+4 -4 4 5 5
+Щоразу я кличу, Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Спаситель, 
+-5 5 -4 -5 5 -4 6 6
+Достойний всій слави навіки 
+4 -4 5 -5 5 -4 -4
+Бо Ти на престолі один!
+4 -4 4 5 -4 4 4
+Щоразу я кличу Ісус!
+
+(Куплет 2)
+
+4 -4 4 5
+У мене є Цар
+4 -4 4 -5 5 -4 4 4
+Що царює над усім
+4 -4 4 5  4 -4 4 -5 5 -4 4 4
+Тримає ключі у святих руках Своїх
+4 -4 4 -5
+Він є вся Міць 
+4 -4 4 -5 5 -4 4 4
+Що руйнує стіни всі
+4 -4 4 -5 5 -4 4 4
+Він воскрес, наш Цар – живий!
+
+(Приспів)
+
+4 -4 4 5 5
+Я кличу Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Цілитель
+-5 5 -4 -5 5 -4 6 6
+Воскрес і царюєш у силі 
+4 -4 5 -5 5 -4 -4
+Серед усіх царств земних
+
+4 -4 4 5 5
+Щоразу я кличу, Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Спаситель, 
+-5 5 -4 -5 5 -4 6 6
+Достойний всій слави навіки 
+4 -4 5 -5 5 -4 -4
+Бо Ти на престолі один!
+4 -4 4 5 -4 4 4
+Щоразу я кличу Ісус!
+
+(Рефрен)
+
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+  4      4    -4     4    4
+Страх - зник! Гріх впав… геть! 
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+4    4   -4    4    4 
+Пек-ло! Трем-тить… все! 
+4 -4 5 -5 5 -4 4
+Щоразу, як кличу Ісус!
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+  4      4    -4     4    4
+Страх - зник! Гріх впав… геть! 
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+45  45   -67  67    45
+Пек-ло! Трем-тить… все! 
+
+(Міст 1 )
+
+5 -5 6  7 -6 6 -5
+Де ж тепер твоя перемога смерть?
+5 5 -5 6 7 -6 6 56
+І де ж твоя, могило, міць?
+5 -5 6 7 -7 -6 6
+Де ж тепер твоя перемога смерть?
+5 5 -5 6 7 -7 -6 67
+І де ж твоя, могило, міць?
+
+(МІст 2)
+
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5  5 -4 -5 5 -5 4
+Мертве ожива в Ім'я Ісуса!
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5  5 -4 -5 5 -5 4
+Мертве ожива в Ім'я Ісуса!
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5  5 -4 -5 5 -5 4
+Мертве ожива в Ім'я Ісуса!
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5 5 5 -4 4 5
+Мертве ожива! 
+5 5 -4 4 5  5 -4 -5 5 -5 4
+Мертве ожива в Ім'я Ісуса!
+
+(Приспів)
+
+4 -4 4 5 5
+Я кличу Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Цілитель
+-5 5 -4 -5 5 -4 6 6
+Воскрес і царюєш у силі 
+4 -4 5 -5 5 -4 -4
+Серед усіх царств земних
+
+4 -4 4 5 5
+Щоразу я кличу, Ісусе
+5 -4 4 5 -4 4 -5 -5
+Бо знаю, що Ти мій Спаситель, 
+-5 5 -4 -5 5 -4 6 6
+Достойний всій слави навіки 
+4 -4 5 -5 5 -4 -4
+Бо Ти на престолі один!
+4 -4 4 5 -4 4 4
+Щоразу я кличу Ісус!
+
+(Рефрен)
+
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+  4      4    -4     4    4
+Страх - зник! Гріх впав… геть! 
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+4    4   -4    4    4 
+Пек-ло! Трем-тить… все! 
+4 -4 5 -5 5 -4 4
+Щоразу, як кличу Ісус!
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+  4      4    -4     4    4
+Страх - зник! Гріх впав… геть! 
+4 -4 5 -5 5 -4          
+Щоразу, як кличу Ісус! 
+45  45   -67  67    45
+Пек-ло! Трем-тить… все!  `
+    },
+    en: {
+        title: "I Know a Name",
+        artist: "Elevation Worship",
+        content: `
+
+(Verse 1)
+        
+4 -4 4 5
+I know a Name
+4 -4 4 -5 5 -4 4 4
+That can silence roaring waves
+4 -4 4 5
+I know a Name
+4 -4 4 -5 5 -4 4 4
+That can empty out a grave
+4 -4 4 5
+I know a Name
+4 -4 4 -5 5 -4 4 4
+It’s the only Name that saves
+4 -4 4 -5 5 -4 4 4
+And it’s worthy of all praise
+
+(Chorus)
+
+4 -4 4 5 5
+I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Healer
+-5 5 -4 -5 5 -4 6 6
+Risen and reigning in power
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+
+4 -4   4 5 5
+Every time I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Savior
+-5 5 -4 -5 5 -4 6 6
+Worthy of glory forever
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+4 -4 4 5 -4 4 4
+Every time I call Your Name
+
+(Verse 2)
+
+4 -4 4 5
+I have a King 
+4 -4 4 -5 5 -4 4 4
+With dominion over death
+4 -4 4 5
+He holds the keys 
+4 -4 4 -5 5 -4 4 4
+In His holy nail-scarred hands
+4 -4 4 5
+He is the heel
+4 -4 4 -5 5 -4 4 4
+That has crushed the serpent's head
+4 -4 4 -5 5 -4 4 4
+Our resurrected Great I Am
+
+(Chorus)
+
+4 -4 4 5 5
+I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Healer
+-5 5 -4 -5 5 -4 6 6
+Risen and reigning in power
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+
+4 -4   4 5 5
+Every time I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Savior
+-5 5 -4 -5 5 -4 6 6
+Worthy of glory forever
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+4 -4 4 5 -4 4 4
+Every time I call Your Name
+
+(Refrain)
+
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+   4     4    -4     4   4
+Chains-break, dry bones wake
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+4    4    -4  4    4 
+The gates of hell shake
+
+4 -4 5 -5 5 -4 4
+Every time I call Your Name
+4         4    -4     4   4
+Chains- break, dry bones wake
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+4    4    -4  4    4 
+The gates of hell shake
+
+(Bridge 1)
+5 -5 6  7 -6 6 -5
+Where oh death is now your sting
+5 5 -5 6 7 -6 6 56
+And where oh grave your victory
+5 -5 6 7 -7 -6 6
+Where oh death is now your sting
+5 5 -5 6 7 -7 -6 67
+And where oh grave your victory
+5 -5 6  7 -6 6 -5
+Where oh death is now your sting
+5 5 -5 6 7 -6 6 56
+And where oh grave your victory
+5 -5 6 7 -7 -6 6
+Where oh death is now your sting
+5 5 -5 6 7 -7 -6 67
+And where oh grave your victory
+5 -5 6  7 -6 6 -5
+Where oh death is now your sting
+5 5 -5 6 7 -6 6 56
+And where oh grave your victory
+5 -5 6 7 -7 -6 6
+Where oh death is now your sting
+5 5 -5 6 7 -7 -6 67
+And where oh grave your victory
+
+
+(Bridge 2)
+
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5  5 -4 -5 5 -5 4
+In the name of Jesus
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5  5 -4 -5 5 -5 4
+In the name of Jesus
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5  5 -4 -5 5 -5 4
+In the name of Jesus
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5 5 5 -4 4 5
+Dead things come alive
+5 5 -4 4 5  5 -4 -5 5 -5 4
+In the name of Jesus
+(Chorus)
+
+4 -4 4 5 5
+I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Healer
+-5 5 -4 -5 5 -4 6 6
+Risen and reigning in power
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+
+4 -4   4 5 5
+Every time I call You Jesus
+5 -4 4 5 -4 4 -5 -5
+I call You, I call You Savior
+-5 5 -4 -5 5 -4 6 6
+Worthy of glory forever
+4 -4 5 -5 5 -4 -4
+Something comes out of the grave
+4 -4 4 5 -4 4 4
+Every time I call Your Name
+
+(Refrain)
+
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+   4     4    -4     4   4
+Chains-break, dry bones wake
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+4    4    -4  4    4 
+The gates of hell shake
+
+4 -4 5 -5 5 -4 4
+Every time I call Your Name
+4         4    -4     4   4
+Chains- break, dry bones wake
+4 -4 5 -5 5 -4          
+Every time I call Your Name
+4    4    -4  4    4 
+The gates of hell shake
+`
+    }
+};
